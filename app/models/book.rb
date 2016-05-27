d7f3d93b3dfc489f4e7cb0a381f7edae0e435d5b
gaspardorey <gaspar.dorey@gmail.com>
2016-05-28 00:11:33 +0100

    Added sale model

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,9 +2,25 @@ class Book < ActiveRecord::Base
 
 	extend FriendlyId
 	friendly_id :name, use: :slugged
-	
+
 	belongs_to :user
+	has_many :sales
+
 	has_attached_file :image 
 	has_attached_file :resource
+
+		validates_numericality_of :price, 
+		greather_then: 49, message: "Must be at least 50 cents"
+		
+	validates_attachment_content_type :image, 
+		content_type:  /^image\/(png|gif|jpeg)/,
+		message: "Only images allowed"
+	validates_attachment_content_type :resource, 
+		content_type: ['application/pdf'],
+		message: "Only pdfs allowed"	
+
+
+	validates :image, attachment_presence: true 
+	validates :resource, attachment_presence: true 
 		
 end
---
+++ db/migrate/20160527230246_create_sales.rb
@@ -0,0 +1,12 @@
+class CreateSales < ActiveRecord::Migration
+  def change
+    create_table :sales do |t|
+      t.string :buyer_email
+      t.string :seller_email
+      t.integer :amount
+      t.string :guid
+
+      t.timestamps
+    end
+  end
+end
